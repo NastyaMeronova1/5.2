255fedfc290e499e222fc52c9684366d4a39c25d
ilimkanu <ilimkanu@gmail.com>
2021-06-30 14:05:52 +0600

    5.2

---
+++ src/Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a=7;
+        int b=42;
+        System.out.println(42+7);
+        System.out.println(42-7);
+        System.out.println(7-42);
+        System.out.println(7*42);
+        System.out.println(7/42);
+        System.out.println(42/7);
+        System.out.println(42%7);
+        System.out.println(7%42);
+        System.out.println(a--);
+        System.out.println(b--);
+        System.out.println(a++);
+        System.out.println(b++);
+    }
+}
